18bb464a0489dac1b1035dab1533990c9f8072e9
curriesandwich <kerryliu0107@gmail.com>
2025-03-15 01:49:41 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 main() {
     clear
     cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle || exit
-
+    
     curl -s "https://raw.githubusercontent.com/curriesandwich/t/main/cool.js" -o "theme-tomorrow_night.js"
 
     img="${1:-https://your-image.com/background.jpg}"
@@ -11,6 +11,7 @@ main() {
     sed -i '' "s|https://i.imgur.com/example.jpeg|$img|g" "theme-tomorrow_night.js"
 
     echo "Background image updated to $img"
+    echo "this was made off of EclipsaSolaria's themes, and i also am not very great with CSS styling so you might face some issues. anyways i tried my best ty"
 }
 
 # Call the function
